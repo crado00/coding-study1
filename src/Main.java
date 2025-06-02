51e0d1134571e8692966139f1f331abad726e1ac
crado00 <alrlawowns@gmail.com>
2025-06-02 13:54:13 +0900

    if statement

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,28 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
+        int number = 5;
 
-        int a = 10, b = 3;
+        if (number > 0) {
+            System.out.println("Number는 0보다 큽니다.");
+        }
+        if (number < 0) {
+            System.out.println("Number는 0보다 작습니다.");
+        }
+        if (number >= 0) {
+            System.out.println("Number는 0보다 크거나 같습니다.");
+        }
+        if (number <= 0) {
+            System.out.println("Number는 0보다 작거나 같습니다.");
+        }
 
-        System.out.println(++a);
+        if (number > 0) {
+            System.out.println("number는 0보다 큽니다.");
+        }else if(number < 0){
+            System.out.println("number는 0보다 작습니다.");
+        }else {
+            System.out.println("number는 0입니다.");
 
-        System.out.println(--b);
-
-        System.out.println(a++);
-
-        System.out.println(b--);
-
-        System.out.println(a);
-
-        System.out.println(b);
+        }
     }
 }
