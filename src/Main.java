25cc65a0ff3345f9473cee723aed05fdf079cb5a
crado00 <alrlawowns@gmail.com>
2025-06-02 11:16:34 +0900

    Increment/decrement operator

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,19 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 3;
 
-        a += 5; // a = a + 5;
+        int a = 10, b = 3;
+
+        System.out.println(++a);
+
+        System.out.println(--b);
+
+        System.out.println(a++);
+
+        System.out.println(b--);
 
         System.out.println(a);
 
-        b -= 1;
-        System.out.println(b);
-        b *= 15;
-        System.out.println(b);
-        b /= 3;
-        System.out.println(b);
-        b %= 7;
         System.out.println(b);
     }
 }
