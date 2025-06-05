88d239dc2e1f36b7093c45aa2adaa997accb1ec3
crado00 <alrlawowns@gmail.com>
2025-06-05 14:34:00 +0900

    function

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,17 @@
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
+
+    public static void printStudent(String name, int age) {
+        System.out.println("이름: " + name);
+        System.out.println("나이: " + age);
+        System.out.println("--------------");
+
+    }
+
     public static void main(String[] args) {
-        int a = 10;
-        int b = 11;
-
-        int max;
-        if (a > b) {
-            max = a;
-        }else {
-            max = b;
-        }
-        System.out.println(max);
-        System.out.println();
-
-        int max2 = (a>b) ? a : b;
-        System.out.println(max2);
-        System.out.println();
-
-        int score = 61;
-        String result;
-
-        if (score > max2) {
-            result = "합격";
-        }else {
-            result = "불합격";
-        }
-        System.out.println(result);
-        System.out.println();
-        String color = "빨강";
-        String choiceColor = (color == "빨강") ? "빨강" : (color == "파랑") ? "파랑" : "보라";
-        System.out.println(choiceColor);
+        System.out.println("===학생 정보 출력===");
+        Main.printStudent("재준", 20);
+        printStudent("charlie", 20);
     }
 }
