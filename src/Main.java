8beb9696bad8345680c83b1ac7b11815ef294f79
crado00 <alrlawowns@gmail.com>
2025-06-05 13:43:21 +0900

    Ternary operator

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,34 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            if (i == 7) {
-                break;
-            }
-            System.out.println(i);
-        }
+        int a = 10;
+        int b = 11;
 
+        int max;
+        if (a > b) {
+            max = a;
+        }else {
+            max = b;
+        }
+        System.out.println(max);
         System.out.println();
 
-        for (int i = 1; i <= 10; i++) {
-            if (i % 3 == 0) {
-                continue;
-            }
-            System.out.println(i);
-        }
-
+        int max2 = (a>b) ? a : b;
+        System.out.println(max2);
         System.out.println();
 
-        for (int i = 1; i <= 20; i++) {
-            if (i % 5 == 0) {
-                continue;
-            }
-            if (i > 15) {
-                break;
-            }
-            System.out.println(i);
+        int score = 61;
+        String result;
+
+        if (score > max2) {
+            result = "합격";
+        }else {
+            result = "불합격";
         }
+        System.out.println(result);
+        System.out.println();
+        String color = "빨강";
+        String choiceColor = (color == "빨강") ? "빨강" : (color == "파랑") ? "파랑" : "보라";
+        System.out.println(choiceColor);
     }
 }
