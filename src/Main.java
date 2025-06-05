8c27c780d9b84ac5c5b7050c4753c0c73f2fe044
crado00 <alrlawowns@gmail.com>
2025-06-05 16:05:50 +0900

    function

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
+import java.util.Scanner;
+
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
 
-    public static void printStudent(String name, int age) {
-        System.out.println("이름: " + name);
-        System.out.println("나이: " + age);
-        System.out.println("--------------");
 
+
+    public static int addNum(int num1, int num2) {
+        return num1 + num2;
     }
-
     public static void main(String[] args) {
-        System.out.println("===학생 정보 출력===");
-        Main.printStudent("재준", 20);
-        printStudent("charlie", 20);
+        int number = addNum(10, 7);
+        int number2 = addNum(5, 6);
+
+        System.out.println(number);
+        System.out.println(number2);
     }
 }
