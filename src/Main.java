80fcc40210839e894501cbae885c0d1287c2c4b5
crado00 <alrlawowns@gmail.com>
2025-06-09 13:51:06 +0900

    array

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,29 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
 
-    public static int add(int a, int b) {
-        return a + b;
-    }
-
-    public static int add(int a, int b, int c) {
-        return a + b + c;
-    }
-
-    public static void greet() {
-        System.out.println("Hello World!");
-    }
-
-    public static void greet(String name) {
-        System.out.println("Hello, " + name + "!");
-    }
-
     public static void main(String[] args) {
-        System.out.println("a + b = " + add(1, 2));
-        System.out.println("a + b + c = " + add(3, 4, 5));
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
 
         System.out.println();
-        greet();
-        greet("Alise");
+
+        /*
+        for (int i = 0; i < 10000; i++) {
+            System.out.println(numbers[i % numbers.length]);
+        }*/
+
+        int[] arr = {2, 4, 6, 8, 10};
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println(sum);
     }
 }
