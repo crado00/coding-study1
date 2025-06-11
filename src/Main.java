84094eb6800df9121d78ffabb68ed3d5c0111967
crado00 <alrlawowns@gmail.com>
2025-06-11 13:36:47 +0900

    Scaner문

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //System.out.print("프린트 함수랑");
-        //System.out.println("프린트 함수 LN 함수의");
-        //System.out.print("차이점을 찾아서");
-        //System.out.println("디스코드 채팅으로");
-        //System.out.print("알려주시면");
-        //System.out.println("좋을것 같습니다.");
+        Scanner scanner = new Scanner(System.in);
 
-        //System.out.println("\"안녕하세요?\"");
 
-        //String name = "Son";
-        //int backNumber = 7;
+        System.out.println("값을 입력하세요.");
+        System.out.println("입력값: " + scanner.nextLine());
 
-        //System.out.println(name + backNumber);
-        //System.out.println((name + backNumber).getClass());
-        int age = 20;
-        double pi = 3.14159;
-
-        System.out.printf("나이: %d, 원주율: %.2f", age, pi);
+        System.out.println("실수값을 입력하세요");
+        System.out.println("입력 실수값:" + scanner.nextDouble());
+        System.out.println("프로그램 종료");
     }
 }
