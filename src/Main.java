173746751214bfd77b84d234ac3c7c0ac0f7cb7a
crado00 <alrlawowns@gmail.com>
2025-06-12 11:47:33 +0900

    override

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //Dog dog = new Dog("뽀뽀", "말티즈");
-        //dog.printName();
+        Dog dog = new Dog("뽀뽀", "말티즈");
+        dog.printName();
 
-        Car car = new Car("현대", "모델은 몰라");
-        car.display();
     }
 
 }
@@ -36,31 +34,9 @@ class Dog extends Animal {
 
 
     }
-}
-
-class Vehicle {
-    String brand;
-
-    public Vehicle(String brand) {
-        this.brand = brand;
-    }
-
-    public void display(){
-        System.out.println("브랜드: " + brand);
-    }
-}
-
-class Car extends Vehicle{
-    String model;
-
-    public Car(String brand, String model){
-        super(brand);
-        this.model = model;
-    }
-
     @Override
-    public void display() {
-        super.display();
-        System.out.println("모델: " + model);
+    public void printName(){
+        System.out.println("Animal name:" + name + " Animal brand: " + brand);
+
     }
 }
