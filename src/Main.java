9a887c89616f46801a408e1da2c77d111f29c456
crado00 <alrlawowns@gmail.com>
2025-06-13 13:44:02 +0900

    다형성

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,89 @@
-class Person{
-    private String name;
-    private int age;
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getName(){
-        return name;
-    }
-    public void setAge(int age){
-        if(age < 18){
-            System.out.println("성인만 가입 가능");
-        }else {
-            this.age = age;
-        }
-    }
-    public int getAge(){
-        return age;
+class Animal {
+    public void makeSound(){
+        System.out.println();
     }
 }
 
-class ReadOnlyStudent {
-    private final int score;
-
-    public ReadOnlyStudent(int score){
-        this.score = score;
+class Dog extends Animal {
+    @Override
+    public void makeSound(){
+        System.out.println("멈멍!");
     }
+}
 
-    public int getScore(){
-        return score;
+class Cat extends Animal {
+    @Override
+    public void makeSound(){
+        System.out.println("야옹야옹!");
+    }
+}
+
+class Bird extends Animal {
+    @Override
+    public void makeSound(){
+        System.out.println("짹짹!");
+    }
+}
+
+class Quokka extends Animal {
+    @Override
+    public void makeSound(){
+        System.out.println("퀔퀔!");
+    }
+}
+
+
+abstract class Shape {
+    final double pi = Math.PI;
+    public abstract  double area();
+}
+
+class Circle extends Shape{
+    int r;
+    public Circle(int r){
+        this.r = r;
+    }
+    @Override
+    public double area() {
+        double area = pi * r * r;
+        return area;
+    }
+}
+
+class Rectangle extends Shape{
+    private double width;
+    private double height;
+    public Rectangle(double width, double height){
+        this.width = width;
+        this.height = height;
+    }
+    @Override
+    public double area() {
+        double area = width * height;
+        return area;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person();
+        //Animal a1 = new Dog();
+        //Animal a2 = new Cat();
+        //Animal a3 = new Bird();
+        //Animal a4 = new Quokka();
 
-        person.setName("crado");
-        person.setAge(20);
+        //a1.makeSound();
+        //a2.makeSound();
+        //a3.makeSound();
+        //a4.makeSound();
 
-        System.out.println(person.getName());
-        System.out.println(person.getAge());
+        Shape[] shapes = new Shape[2];
+
+        shapes[0] = new Circle(3);
+        shapes[1] = new Rectangle(4, 5);
+
+        for(Shape shape : shapes){
+            System.out.println("도형의 넓이는" + shape.area() + "입니다.");
+        }
 
     }
 }
