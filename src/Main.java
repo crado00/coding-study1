fa0056bb66842cc4c95c488910e3df5abaad3543
crado00 <alrlawowns@gmail.com>
2025-06-13 11:27:24 +0900

    getter setter

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,47 @@
 class Person{
+    private String name;
+    private int age;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setAge(int age){
+        if(age < 18){
+            System.out.println("성인만 가입 가능");
+        }else {
+            this.age = age;
+        }
+    }
+    public int getAge(){
+        return age;
+    }
 }
+
+class ReadOnlyStudent {
+    private final int score;
+
+    public ReadOnlyStudent(int score){
+        this.score = score;
+    }
+
+    public int getScore(){
+        return score;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
+        Person person = new Person();
+
+        person.setName("crado");
+        person.setAge(20);
+
+        System.out.println(person.getName());
+        System.out.println(person.getAge());
+
     }
 }
 
