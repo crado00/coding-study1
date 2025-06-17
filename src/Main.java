37edc5e0c58b26b8be352adc94f39baf8ea34907
crado00 <alrlawowns@gmail.com>
2025-06-17 14:47:14 +0900

    finally

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,20 @@
 
 public class Main {
     public static void main(String[] args) {
-        String input = "2,a34";
+
+        int a = 10;
+        int b = 2;
 
         try {
-            int result = multiply(input);
+            int result = a / b;
 
-            System.out.println(result);
-
-        }catch (NumberFormatException e) {
-            System.out.println(e.getMessage());
-            System.out.println("에러!");
+            System.out.println("result: " + result);
+        }catch (ArithmeticException e){
+            System.out.println("Error: 0으로 나눔");
+        }finally {
+            System.out.println("Cleanup: 이 블록은 항상 실행됩니다.");
         }
-
     }
 
-    public static int multiply(String input) throws NumberFormatException {
-        if(input.length() != 2){
-            throw new NumberFormatException("숫자 두개 여야함");
-        }
-        int inputA = Integer.parseInt(input.substring(0,1));
-        int inputB = Integer.parseInt(input.substring(1));
-
-        return inputA * inputB;
-
-    }
 }
 
