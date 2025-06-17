1e16d7211bdd3aee0db5b77d70132a7f066eea11
crado00 <alrlawowns@gmail.com>
2025-06-17 13:31:33 +0900

    throw

--- src/Main.java
+++ src/Main.java
@@ -1,144 +1,17 @@
-/* 2번째 확인
-class Car {
-    String brand;
-    Engine engine;
-    public Car(String brand){
-        this.brand = brand;
-        Engine engine = new Engine();
-        this.engine = engine;
-    }
 
-    class Engine {
-        public void start(){
-            System.out.println("the engine of " + brand + "is starting.");
-        }
-    }
-
-    public void startEngine() {
-        engine.start();
-    }
-}
-
- */
-
-/*4번째 확인
-class Outer {
-    private class HiddenInner {
-        void secret(){
-            System.out.println("private!");
-        }
-
-    }
-    public void reveal() {
-        HiddenInner h1 = new HiddenInner();
-        h1.secret();;
-    }
-}
- */
 public class Main {
-    /* 1번째 확인
-    private String message = "Hello from the outer class.";
-    public class Inner {
-        void displayMessage() {
-            System.out.println("Inner says: " + message);
-        }
-    }
-
-    public void callInner(){
-        Inner inner = new Inner();
-        inner.displayMessage();
-    }
-
-     */
-/*3번째 확인
-    String outerMessage = "Hello from Outer";
-
-    class Inner {
-        void showMessage() {
-            System.out.println(outerMessage);
-        }
-    }
- */
-    /*5번째 확인
-    interface OnClickListener {
-        void onClick();
-    }
-
-    private OnClickListener listener;
-
-    public void setOnClickListener(OnClickListener listener) {
-        this.listener = listener;
-    }
-
-    public void click() {
-        if(listener != null) {
-            listener.onClick();
-        }
-    }
-
-    private class ClickHandler implements OnClickListener {
-        @Override
-        public void onClick() {
-            System.out.println("Button was clicked!");
-        }
-    }
-
-    public void simulate() {
-        setOnClickListener(new ClickHandler());
-        click();
-    }
-
-     */
-/*6번째 확인
-    private String data = "Outer data";
-
-    class  Inner {
-        void printData() {
-            System.out.println(data);
-        }
-    }
-
-    public Inner createInner() {
-        return new Inner();
-    }
- */
-
-    class Outer {
-        static class StaticInner{
-            void hello () {
-            System.out.println("Hi");
-        }
-        }
-    }
     public static void main(String[] args) {
-        //Outer outer = new Outer(); 1번째 확인
-        //outer.callInner(); 1번째 확인
+        String string = "123a";
 
-        //Car car = new Car("자동차"); 2번째 확인
-        //car.startEngine(); 2번째 확인
+        try {
+            int num = convertToInt(string);
+        }catch (NumberFormatException e) {
+            System.out.println("숫자 형식 오류");
+        }
+    }
 
-        //Main main = new Main();3번째 확인
-        //Main.Inner inner = main.new Inner();3번째 확인
-
-        //inner.showMessage();3번째 확인
-        //Outer outer = new Outer();4번째 확인
-
-        //Main btn = new Main();5번째 확인
-        //btn.simulate();5번째 확인
-/*6번째 확인
-        Main outer = new Main();
-        Main.Inner inner = outer.createInner();
-        inner.printData();
-
-        outer = null;
-        inner.printData();
-        inner = null;
-        inner.printData();
-
-
- */
-
-        Outer.StaticInner inner = new Outer.StaticInner();
-        inner.hello();
+    public static int convertToInt(String s) throws NumberFormatException{
+        return Integer.parseInt(s);
     }
 }
+
