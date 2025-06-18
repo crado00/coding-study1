ced51752cba033430fd6b2e9183a9fd11e585adf
crado00 <alrlawowns@gmail.com>
2025-06-18 14:06:21 +0900

    TreeSet

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Set<String> fruits = new LinkedHashSet<>();
+        Set<String> fruits = new TreeSet<>();
 
         fruits.add("Banana");
         fruits.add("Apple");
