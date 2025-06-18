d5312b9173e332b71fa4fd5e117d03f7227c3717
crado00 <alrlawowns@gmail.com>
2025-06-18 14:05:49 +0900

    LinkedHashSet

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,15 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Set<String> fruits = new HashSet<>();
+        Set<String> fruits = new LinkedHashSet<>();
 
-        fruits.add("Apple");
-        fruits.add("Apple");
         fruits.add("Banana");
+        fruits.add("Apple");
+
+
         for (String fruit:fruits) {
-            System.out.println(fruit);
+            System.out.println(fruit.hashCode());
         }
+
     }
 }
