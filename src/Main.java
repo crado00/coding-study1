1f563f9e5dca5f8e0f54c6af93151083ba7fcf9b
crado00 <alrlawowns@gmail.com>
2025-06-19 11:41:35 +0900

    Generics 1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,45 @@
 import java.util.*;
+//제너릭 클래스
+class  Box<T> {
+    private T constent;
 
+    public Box(T constent) {
+        this.constent = constent;
+    }
+
+    public T getConstent(){
+        return constent;
+    }
+
+    public void setConstent(T constent){
+        this.constent = constent;
+    }
+
+    public <E> void printContent(E extranInfo) {
+        System.out.println("content: " + constent + " Extra: " + extranInfo);
+    }
+}
+//기존 생성자
+class StringBox {
+    private String content;
+
+    public  StringBox(String content) {
+        this.content = content;
+    }
+}
+
+class IntegerBox {
+    private Integer content;
+}
 public class Main {
     public static void main(String[] args) {
-        Map<String, Integer> fruitPrices = new TreeMap<>();
 
-        fruitPrices.put("사과", 17980);
-        fruitPrices.put("배", 10320);
-        fruitPrices.put("멜론", 10630);
-        fruitPrices.put("수박", 15920);
+        Box<String> stringBox = new Box<String>("나는 커서 훌륭한 박스가 될거야!");
 
-        for (Map.Entry<String, Integer> entry: fruitPrices.entrySet()){
-            System.out.println(entry.getKey()+entry.getValue());
-        }
+        System.out.println(stringBox.getConstent());
+        stringBox.setConstent("실패!");
+        System.out.println(stringBox.getConstent());
 
+        stringBox.printContent(123);
     }
 }
