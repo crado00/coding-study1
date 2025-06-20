a2ae10516f074e48999ab5a2ef525f565e77b256
crado00 <alrlawowns@gmail.com>
2025-06-20 10:49:48 +0900

    stream 2

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,12 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
-        Stream<Integer> stream = numbers.stream();
+        List<Double> values = Arrays.asList(10.0, 20.0, 30.0, 40.0, 50.0);
 
-        int result = numbers.stream()
-                .filter((n ) -> n % 2 == 0)
-                .mapToInt(Integer::intValue)
-                .sum();
-        System.out.println(result);
-        /*
-        System.out.println();
-        int sum = 0;
-        for (int i = 0; i < numbers.size(); i ++){
-            if(numbers.get(i) % 2 == 0){
-                sum += numbers.get(i);
-            }
-        }
-        System.out.println(sum);
+        double avg = values.stream()
+                .mapToDouble(Double::doubleValue)
+                .average().orElse(0.0);
 
-         */
+        System.out.println(avg);
     }
 }
